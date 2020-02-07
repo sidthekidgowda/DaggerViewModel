f148fe5320edfd346969ce2a9de85b2e61ca0284
Sid Gowda <sidgowda.9@gmail.com>
2020-02-06 22:06:55 -0500

    set up dagger

--- app/src/main/java/di/ViewModelModule.kt
+++ app/src/main/java/di/ViewModelModule.kt
@@ -1,9 +1,15 @@
 package di
 
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
 import dagger.Module
+import viewmodel.ViewModelFactory
 
 @Module
 abstract class ViewModelModule {
+
+    @Binds
+    abstract fun bindsViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
 
 
 }
